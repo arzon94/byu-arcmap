76676b547d7f1bf5a54cc54b78e2aeb7d685508b
Andrew Perkins <andyp3rkins@gmail.com>
2017-10-06 13:56:36 -0600

    removed dojo build, added aedlocations, added color to each featurelayer item in LayerList, changed parking layers to the updated parking layers, changed initializing parking lots featurelayers to a featurelayer array

--- scripts/map.js
+++ scripts/map.js
@@ -1,4 +1,7 @@
-var map, view, layerList, legend;
+var map,
+  view,
+  layerList,
+  legend;
 var featureLayerIDSet = [];
 
 function initialize() {
@@ -19,103 +22,90 @@ function initialize() {
     view = new MapView({
       container: "viewDiv",
       map: map,
-      center: [-111.649278, 40.249251],
-      zoom: 16,
-
+      center: [
+        -111.649278, 40.249251
+      ],
+      zoom: 16
     });
     view.then(function() {
       toggleBuildings();
 
-        var template = {
-          title: "{Name}",
-          content: "{Description}",
-          overwriteActions: true
-        };
-        var searchWidget = new Search({
-          view: view,
-          allPlaceholder: "Enter Name or Acronym",
-          sources: [{
-              featureLayer: new FeatureLayer({
-                url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/Buildings_Edited/FeatureServer/0",
-                popupTemplate: template
-              }),
-              searchFields: ["Name", "BLDG_ABBR"],
-              suggestionTemplate: "{Name} ({BLDG_ABBR})",
-              displayField: "Name",
-              exactMatch: false,
-              outFields: ["Name", "BLDG_ABBR", "Description"],
-              name: "Buildings",
-              placeholder: "example: JSB",
-            }, {
-              featureLayer: new FeatureLayer({
-                url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/Athletics/FeatureServer/0",
-                popupTemplate: template
-              }),
-              searchFields: ["Name", "BLDG_ABBR"],
-              suggestionTemplate: "{Name}",
-              exactMatch: false,
-              outFields: ["*"],
-              name: "Sports",
-              placeholder: "example: Marriott Center",
-            },
-            {
-              featureLayer: new FeatureLayer({
-                url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/Services/FeatureServer/0",
-                popupTemplate: template
-              }),
-              searchFields: ["Name"],
-              suggestionTemplate: "{Name}",
-              exactMatch: false,
-              outFields: ["*"],
-              name: "Services",
-              placeholder: "example: Admissions",
-            }, {
-              featureLayer: new FeatureLayer({
-                url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/StudentServices/FeatureServer/0",
-                popupTemplate: template
-              }),
-              searchFields: ["Name"],
-              suggestionTemplate: "{Name}",
-              exactMatch: false,
-              outFields: ["*"],
-              name: "Student Services",
-              placeholder: "example: Title IX",
-            }, {
-              featureLayer: new FeatureLayer({
-                url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/Entertainment_Museums/FeatureServer/0",
-                popupTemplate: template
-              }),
-              searchFields: ["Name"],
-              suggestionTemplate: "{Name}",
-              exactMatch: false,
-              outFields: ["*"],
-              name: "Entertainment and Museums",
-              placeholder: "example: Planetarium",
-            }
-          ]
-        });
-
-        view.ui.add(searchWidget, {
-          position: "top-left",
-          index: 2
-        });
-        view.ui.move("zoom", "top-left");
-        var homeWidget = new Home({
-          view: view
-        });
-        view.ui.add(homeWidget, "top-left");
-        view.popup.dockEnabled = false;
-        searchWidget.then(function() {
-
-        }, function(error) {
-          console.log('search failed', error);
-        });
-
-      },
-      function(error) {
-        // Use the errback function to handle when the view doesn't load properly
-        console.log('The view\'s resources failed to load: ', error);
+      var template = {
+        title: "{Name}",
+        content: "{Description}",
+        overwriteActions: true
+      };
+      var searchWidget = new Search({
+        view: view,
+        allPlaceholder: "Enter Name or Acronym",
+        sources: [
+          {
+            featureLayer: new FeatureLayer({url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/Buildings_Edited/FeatureServer/0", popupTemplate: template}),
+            searchFields: [
+              "Name", "BLDG_ABBR"
+            ],
+            suggestionTemplate: "{Name} ({BLDG_ABBR})",
+            displayField: "Name",
+            exactMatch: false,
+            outFields: [
+              "Name", "BLDG_ABBR", "Description"
+            ],
+            name: "Buildings",
+            placeholder: "example: JSB"
+          }, {
+            featureLayer: new FeatureLayer({url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/Athletics/FeatureServer/0", popupTemplate: template}),
+            searchFields: [
+              "Name", "BLDG_ABBR"
+            ],
+            suggestionTemplate: "{Name}",
+            exactMatch: false,
+            outFields: ["*"],
+            name: "Sports",
+            placeholder: "example: Marriott Center"
+          }, {
+            featureLayer: new FeatureLayer({url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/Services/FeatureServer/0", popupTemplate: template}),
+            searchFields: ["Name"],
+            suggestionTemplate: "{Name}",
+            exactMatch: false,
+            outFields: ["*"],
+            name: "Services",
+            placeholder: "example: Admissions"
+          }, {
+            featureLayer: new FeatureLayer({url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/StudentServices/FeatureServer/0", popupTemplate: template}),
+            searchFields: ["Name"],
+            suggestionTemplate: "{Name}",
+            exactMatch: false,
+            outFields: ["*"],
+            name: "Student Services",
+            placeholder: "example: Title IX"
+          }, {
+            featureLayer: new FeatureLayer({url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/Entertainment_Museums/FeatureServer/0", popupTemplate: template}),
+            searchFields: ["Name"],
+            suggestionTemplate: "{Name}",
+            exactMatch: false,
+            outFields: ["*"],
+            name: "Entertainment and Museums",
+            placeholder: "example: Planetarium"
+          }
+        ]
       });
+
+      view.ui.add(searchWidget, {
+        position: "top-left",
+        index: 2
+      });
+      view.ui.move("zoom", "top-left");
+      var homeWidget = new Home({view: view});
+      view.ui.add(homeWidget, "top-left");
+      view.popup.dockEnabled = false;
+      searchWidget.then(function() {}, function(error) {
+        console.log('search failed', error);
+      });
+
+    }, function(error) {
+      // Use the errback function to handle when the view doesn't load properly
+      console.log('The view\'s resources failed to load: ', error);
+    });
   });
 }
 
@@ -125,17 +115,17 @@ function toggleLayers(id) {
     removeLayers();
   }
   require([
-    "esri/layers/FeatureLayer",
-    "esri/tasks/support/Query",
-    "dojo/domReady!"
+    "esri/layers/FeatureLayer", "esri/tasks/support/Query", "dojo/domReady!"
   ], function(FeatureLayer, Query) {
     //load ParkingLayers
     var template = {
       title: "{Name}",
-      content: [{
-        type: "text",
-        text: "<img src='{ImageUrl}'><div class='popupText'>{Description}</div>   <a target='_blank' href='{url}'>{url}</a>"
-      }]
+      content: [
+        {
+          type: "text",
+          text: "<img src='{ImageUrl}'><div class='popupText'>{Description}</div>   <a target='_blank' href='{url}'>{url}</a>"
+        }
+      ]
 
     };
     var featureLayer = new FeatureLayer({
@@ -214,20 +204,18 @@ function toggleLayers(id) {
       var resultId = target.getAttribute("data-result-id");
 
       // get the graphic corresponding to the clicked zip code
-      var result = resultId && graphics && graphics[parseInt(resultId,
-        10)];
+      var result = resultId && graphics && graphics[parseInt(resultId, 10)];
 
       if (result) {
-        var centerPoint = (featureLayer.title == "Buildings Edited" || featureLayer.title == "Athletics") ? result.geometry.centroid : {
-          latitude: result.geometry.latitude,
-          longitude: result.geometry.longitude
-        };
+        var centerPoint = (featureLayer.title == "Buildings Edited" || featureLayer.title == "Athletics")
+          ? result.geometry.centroid
+          : {
+            latitude: result.geometry.latitude,
+            longitude: result.geometry.longitude
+          };
         // open the popup at the centroid of zip code polygon
         // and set the popup's features which will populate popup content and title.
-        view.popup.open({
-          features: [result],
-          location: centerPoint
-        });
+        view.popup.open({features: [result], location: centerPoint});
       }
     }
   });
@@ -239,9 +227,7 @@ function toggleLegendLayers(id) {
     removeLayers();
   }
   require([
-    "esri/layers/FeatureLayer",
-    "esri/widgets/Legend",
-    "dojo/domReady"
+    "esri/layers/FeatureLayer", "esri/widgets/Legend", "dojo/domReady"
   ], function(FeatureLayer, Legend) {
     var template = {
       title: "{Name}",
@@ -251,7 +237,9 @@ function toggleLegendLayers(id) {
     if (id === "ComputerLabs_Merge" || id === "Transportaion_Merge") {
       featureLayer = new FeatureLayer({
         url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/" + id + "/FeatureServer/0",
-        outFields: ["Name", "Description", "StopLocati"],
+        outFields: [
+          "Name", "Description", "StopLocati"
+        ],
         popupTemplate: template
       });
     } else {
@@ -266,9 +254,11 @@ function toggleLegendLayers(id) {
       console.log(featureLayer);
       legend = new Legend({
         view: view,
-        layerInfos: [{
-          layer: featureLayer
-        }]
+        layerInfos: [
+          {
+            layer: featureLayer
+          }
+        ]
       });
       view.ui.add(legend, "bottom-left");
     });
@@ -281,8 +271,7 @@ function toggleBuildings() {
     removeLayers();
   }
   require([
-    "esri/layers/FeatureLayer",
-    "dojo/domReady!"
+    "esri/layers/FeatureLayer", "dojo/domReady!"
   ], function(FeatureLayer) {
     //load ParkingLayers
     var template = {
@@ -291,7 +280,9 @@ function toggleBuildings() {
     };
     var featureLayer = new FeatureLayer({
       url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/Buildings_Edited/FeatureServer/0",
-      outFields: ["Name", "Description"],
+      outFields: [
+        "Name", "Description"
+      ],
       popupTemplate: template,
       opacity: 0
     });
@@ -307,25 +298,37 @@ function toggleParkingLots() {
     removeLayers();
   }
   require([
-    "esri/layers/FeatureLayer",
-    "esri/widgets/LayerList",
-    "dojo/domReady!"
-  ], function(FeatureLayer, LayerList) {
+    "esri/layers/FeatureLayer", "esri/widgets/LayerList", "dojo/query", "dojo/domReady!"
+  ], function(FeatureLayer, LayerList, query) {
     //load ParkingLayers
     var template = {
       title: "{Lot}",
       content: "{Description}"
     };
-    for (var i = 2; i < 12; i++) {
-      var featureLayer = new FeatureLayer({
-        url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/ParkingLayers/FeatureServer/" + i,
-        outFields: ["Lot", "Description", "Map_Category"],
+    var parkingLotsArrayNames = [
+      "MotorcycleParking",
+      "Students",
+      "RestrictedVisitor",
+      "Construction",
+      "StudentHousing",
+      "FreeParking",
+      "VisitorParking",
+      "TimedParking",
+      "GraduateStudents",
+      "FacultyandStaff"
+    ];
+    var featureLayerArray = [];
+    for (var i = 0; i < 10; i++) {
+      featureLayerArray[i] = new FeatureLayer({
+        url: "https://services.arcgis.com/FvF9MZKp3JWPrSkg/arcgis/rest/services/" + parkingLotsArrayNames[i] + "/FeatureServer/0",
+        outFields: [
+          "Lot", "Description", "Map_Category"
+        ],
         popupTemplate: template
       });
       //console.log(featureLayer.title);
-      map.add(featureLayer);
-      // console.log(featureLayer.layerId);
-      featureLayerIDSet.push(featureLayer.id);
+      map.add(featureLayerArray[i]);
+      featureLayerIDSet.push(featureLayerArray[i].id);
     }
     layerList = new LayerList({
       view: view,
@@ -333,38 +336,39 @@ function toggleParkingLots() {
       listItemCreatedFunction: function(event) {
         //console.log(event);
         var item = event.item;
-        item.title = item.title.substr(16);
-        //open the list item in the LayerList
-        // item.open = true;
-        // item.actionsSections = [
-        //   [{
-        //     title: "Show/Hide Layer",
-        //     className: "esri-icon-radio-checked",
-        //     id: "full-extent"
-        //   }]
-        // ];
-        // if (item.title === "ParkingLayers - Construction") {
-        //   item.title = "Construction";
-        // } else if (item.title === "ParkingLayers - Faculty and Staff") {
-        //   item.title = "Faculty and Staff";
-        // } else if (item.title === "ParkingLayers - Free Parking") {
-        //   item.title = "Free Parking";
-        // } else if (item.title === "ParkingLayers - Free Parking") {
-        //   item.title = "Free Parking";
-        // } else if (item.title === "ParkingLayers - Free Parking") {
-        //   item.title = "Free Parking";
-        // } else if (item.title === "ParkingLayers - Free Parking") {
-        //   item.title = "Free Parking";
-        // } else if (item.title === "ParkingLayers - Free Parking") {
-        //   item.title = "Free Parking";
-        // }
+        setTimeout(function() {
+          //var item = event.item;
+          var itemUid = layerList.id + "_" + item.uid + "__title";
+          var node = query("[aria-labelledby=\"" + itemUid + "\"]")[0];
+          if (node && node.children[0] && item.layer.renderer && item.layer.renderer.symbol) {
+            node.children[0].style.backgroundColor = item.layer.renderer.symbol.color;
+
+            if (item.title === "FacultyandStaff") {
+              item.title = "Faculty and Staff";
+            } else if (item.title === "FreeParking") {
+              item.title = "Free Parking";
+            } else if (item.title === "MotorcycleParking") {
+              item.title = "Motorcycle Parking";
+            } else if (item.title === "RestrictedVisitor") {
+              item.title = "Restricted Visitor";
+            } else if (item.title === "StudentHousing") {
+              item.title = "Student Housing";
+            } else if (item.title === "TimedParking") {
+              item.title = "Timed Parking";
+            } else if (item.title === "VisitorParking") {
+              item.title = "Visitor Parking";
+            } else if (item.title === "GraduateStudents") {
+              item.title = "Graduate Students";
+            } else if (item.title === "Students") {
+              node.children[0].children[0].children[1].style.color = "unset";
+            }
+          }
+        }, 50);
+
       }
     });
 
-
-    view.ui.add(layerList, {
-      position: "bottom-left"
-    });
+    view.ui.add(layerList, {position: "bottom-left"});
   });
 }
 
@@ -374,33 +378,101 @@ function toggleAEDs() {
     "esri/layers/FeatureLayer",
     "esri/request",
     "esri/config",
+    "esri/geometry/Point",
+    "esri/widgets/Legend",
     "dojo/domReady!"
-  ], function(FeatureLayer, esriRequest, esriConfig) {
+  ], function(FeatureLayer, esriRequest, esriConfig, Point, Legend) {
     var aedurl = "https://risk.byu.edu/ws/aedfeed.php";
-    // var xhttp = new XMLHttpRequest();
-    // xhttp.onreadystatechange = function() {
-    //   console.log(this);
-    // };
-    // xhttp.open("GET", aedurl, true);
-    // xhttp.send();
 
-    //console.log(esriConfig.request);
-    esriConfig.request.proxyUrl = aedurl;
-    var options = {
-      responseType: 'json',
-      useProxy: true
-    };
-    esriRequest(aedurl, options).then(function(response) {
-      console.log('response', response);
-      var responseJSON = JSON.stringify(response, null, 2);
-      console.log(responseJSON);
-    }, function(error) {
-      console.log('request failed', error);
-    });
+    getData().then(createGraphics). // then send it to the createGraphics() method
+    then(createLayer). // when graphics are created, create the layer
+    otherwise(errback);
+    //attempt to access the aedlive feed. if not, return the local aedfeed.json
+    function getData() {
+      return esriRequest(aedurl).then(function(response) {
+        console.log("response");
+        return response;
+      }).otherwise(function(error) {
+        console.log('request failed, grabbing local feed');
+        return esriRequest("scripts/aedfeed.json", {responseType: "json"});
+      });
+    }
+    function createGraphics(response) {
+      // raw GeoJSON data
+      var geoJson = response.data;
+      // Create an array of Graphics from each GeoJSON feature
+      return geoJson.features.map(function(feature, i) {
+        return {
+          geometry: new Point({x: feature.geometry.coordinates[0], y: feature.geometry.coordinates[1]}),
+          // select only the attributes you care about
+          attributes: {
+            ObjectID: i,
+            building: feature.properties.building,
+            room: feature.properties.room,
+            location: feature.properties.location,
+            picture: feature.properties.picture
+          }
+        };
+      });
+    }
+    function createLayer(graphics) {
 
-    // var featureLayer = new FeatureLayer({
-    //
-    // });
+      var fields = [
+        {
+          name: "ObjectID",
+          alias: "ObjectID",
+          type: "oid"
+        }, {
+          name: "building",
+          alias: "building",
+          type: "string"
+        }, {
+          name: "room",
+          alias: "room",
+          type: "string"
+        }, {
+          name: "location",
+          alias: "location",
+          type: "string"
+        }, {
+          name: "picture",
+          alias: "picture",
+          type: "string"
+        }
+      ];
+      var template = {
+        title: "AED #{ObjectID}",
+        content: "<img src='{picture}' height='320' width='240'/><div class='popupText'>{building} room {room}  {location} </div>"
+      };
+      featureLayer = new FeatureLayer({
+        source: graphics, // autocast as an array of esri/Graphic
+        // create an instance of esri/layers/support/Field for each field object
+        fields: fields, // This is required when creating a layer from Graphics
+        objectIdField: "ObjectID", // This must be defined when creating a layer from Graphics
+        renderer: {
+          type: "simple", // autocasts as new SimpleRenderer()
+          symbol: {
+            type: "picture-marker", // autocasts as new SimpleMarkerSymbol()
+            url: "aed.png",
+            width: "30px",
+            height: "56px"
+          }
+        },
+        spatialReference: {
+          wkid: 4326
+        },
+        popupTemplate: template,
+        geometryType: "point", // Must be set when creating a layer from Graphics
+      });
+
+      map.add(featureLayer);
+      featureLayerIDSet.push(featureLayer.id);
+      return featureLayer;
+    }
+    function createLegend(layer) {}
+    function errback(error) {
+      console.error("Creating failed. ", error);
+    }
   });
 }
 
